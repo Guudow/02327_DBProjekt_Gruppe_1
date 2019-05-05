9279a67f2596bd8dfc85fb7348d51f2e0da343b0
guudow <mguudow@aol.com>
2019-05-05 15:07:25 +0200

    Igang med DAO classer til at implementere

---
+++ src/DAL/DAO/IIngredientDAO.java
@@ -0,0 +1,7 @@
+package DAL.DAO;
+
+public interface IIngredientDAO {
+
+
+
+}
---
+++ src/DAL/DAO/UserDAO.java
@@ -0,0 +1,47 @@
+package DAL.DAO;
+
+import DAL.DTO.IUser;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class UserDAO implements IUserDAO {
+
+    private Connection connection;
+
+    public UserDAO(Connection connection) throws DALException{
+        this.connection = connection;
+    }
+
+    @Override
+    public void createUser(IUser userID, IUser user) throws DALException {
+
+
+        if (userID.equals(user) && user.getRolesID().contains("admin")) {
+            throw new DALException("roles contain admin");
+        }
+
+
+
+    }
+
+    @Override
+    public IUser getUser(int userID) throws DALException {
+        return null;
+    }
+
+    @Override
+    public List<IUser> getUserList() throws DALException {
+        return null;
+    }
+
+    @Override
+    public void updateUser(IUser user) throws DALException {
+
+    }
+
+    @Override
+    public void deleteUser(int userId) throws DALException {
+
+    }
+}
